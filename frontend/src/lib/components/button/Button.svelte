2c11b8013fa6e82971673465ea0b049cbf6c4638
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-09-16 21:19:35 +0700

    feat: create button component

---
+++ frontend/src/lib/components/button/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Button } from "bits-ui";
+	import type { Snippet } from "svelte";
+    export type ButtonProps = {
+        children: Snippet
+        onClick: () => void
+    }
+
+    let {children, onClick} : ButtonProps = $props()
+</script>
+
+<Button.Root onclick={onClick} class="rounded-md bg-[#365486] p-2 font-light text-sm text-[#DCF2F1] border border-[#365486] hover:border-[#7FC7D9]">
+    {@render children()}
+</Button.Root>
---
+++ frontend/src/routes/playground/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Button from "$lib/components/button/Button.svelte";
+    import toast, { Toaster } from 'svelte-french-toast';
+</script>
+
+<div>
+    <Button onClick={() => {}}>
+        Button Example
+    </Button>
+    <Button onClick={() => {toast.success("Success!", {
+        position: "top-right"
+    })}}>
+        Click for success toast
+    </Button>
+</div>
+
+<Toaster />
