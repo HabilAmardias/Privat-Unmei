6310f620e08aa2f58d6476671c9345f30f117180
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-10-16 07:52:13 +0700

    feat: admin profile page design (WIP)

--- frontend/src/routes/(manager-app)/manager/admin/+page.svelte
+++ frontend/src/routes/(manager-app)/manager/admin/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import type { PageProps } from './$types';
 	import { goto } from '$app/navigation';
+	import CldImage from '$lib/components/image/CldImage.svelte';
 
 	let { data }: PageProps = $props();
 	onMount(() => {
@@ -10,3 +11,15 @@
 		}
 	});
 </script>
+
+<svelte:head>
+	<title>My Profile - Privat Unmei</title>
+	<meta name="description" content="My Profile - Privat Unmei" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</svelte:head>
+
+<div class="flex h-full flex-col gap-4 p-4">
+	<div class="flex items-center gap-4">
+		<CldImage src={data.profile.profile_image} className="rounded-full" />
+	</div>
+</div>
