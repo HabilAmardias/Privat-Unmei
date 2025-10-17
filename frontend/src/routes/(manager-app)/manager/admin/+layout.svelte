fe41733f694aa227b4db7393c324288bde15f7ff
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-10-17 07:53:41 +0700

    feat: more nav addition for admin pages

--- frontend/src/routes/(manager-app)/manager/admin/+layout.svelte
+++ frontend/src/routes/(manager-app)/manager/admin/+layout.svelte
@@ -5,8 +5,12 @@
 	import { goto } from '$app/navigation';
 	import Link from '$lib/components/button/Link.svelte';
 	import { ScrollArea } from 'bits-ui';
-	import { House, Menu } from '@lucide/svelte';
+	import { House, LogOut, Menu, Percent, User } from '@lucide/svelte';
 	import Button from '$lib/components/button/Button.svelte';
+	import Menubar from '$lib/components/menubar/Menubar.svelte';
+	import MenuItem from '$lib/components/menubar/MenuItem.svelte';
+	import toast from 'svelte-french-toast';
+	import { loadingStore } from '$lib/stores/LoadingStore.svelte';
 
 	const { children, data }: LayoutProps = $props();
 
@@ -19,39 +23,90 @@
 	function handleMenu() {
 		menuOpen = !menuOpen;
 	}
+	function onLogout() {
+		const loadID = toast.loading('logging out....', { position: 'top-right' });
+		loadingStore.setLogOutLoadID(loadID);
+		goto('/manager/logout', { replaceState: true });
+	}
 </script>
 
-<main class="flex">
+<main class="flex h-dvh">
 	<nav
 		class="
-            flex h-dvh flex-col items-center gap-4 bg-[var(--tertiary-color)] p-4
-            transition-all duration-300 ease-in-out
-            {menuOpen ? 'w-[30%] md:w-[15%]' : 'w-20'} 
+            hidden h-dvh flex-col items-center gap-4 bg-[var(--tertiary-color)] p-4 transition-all
+            duration-300 ease-in-out md:flex
+            {menuOpen ? 'w-[15%]' : 'w-20'} 
             "
 	>
 		<div class="mb-16 w-full justify-self-start">
-			<Button onClick={handleMenu} full={true}>
+			<Button onClick={handleMenu} full>
 				<Menu class="duration-300 ease-in-out {menuOpen ? 'rotate-90' : ''}" />
 			</Button>
 		</div>
-
 		<Link href="/manager/admin">
 			<div class="flex flex-col items-center gap-1">
 				<House />
 				<p class="duration-300 ease-in-out {menuOpen ? 'opacity-100' : 'opacity-0'}">Home</p>
 			</div>
 		</Link>
-
-		<Link href="/manager/admin">
+		<Link href="/manager/admin/mentors">
 			<div class="flex flex-col items-center gap-1">
-				<House />
-				<p class="duration-300 ease-in-out {menuOpen ? 'opacity-100' : 'opacity-0'}">Home</p>
+				<User />
+				<p class="duration-300 ease-in-out {menuOpen ? 'opacity-100' : 'opacity-0'}">Mentors</p>
 			</div>
 		</Link>
+		<Link href="/manager/admin/maintenance">
+			<div class="flex flex-col items-center gap-1">
+				<Percent />
+				<p class="duration-300 ease-in-out {menuOpen ? 'opacity-100' : 'opacity-0'}">Maintenance</p>
+			</div>
+		</Link>
+		<Button onClick={onLogout} withBg={false} textColor="light" withPadding={false}>
+			<div class="flex flex-col items-center gap-1">
+				<LogOut />
+				<p class="duration-300 ease-in-out {menuOpen ? 'opacity-100' : 'opacity-0'}">Logout</p>
+			</div>
+		</Button>
 	</nav>
-	<ScrollArea.Root class="h-full flex-1">
-		<ScrollArea.Viewport class="h-full flex-1">
+	<ScrollArea.Root class="flex-1">
+		<ScrollArea.Viewport class="h-full">
 			{@render children()}
 		</ScrollArea.Viewport>
 	</ScrollArea.Root>
+	<div class="block md:hidden">
+		<Menubar>
+			<MenuItem>
+				<Link href="/manager/admin">
+					<div class="flex flex-col items-center gap-1">
+						<House />
+						<p>Home</p>
+					</div>
+				</Link>
+			</MenuItem>
+			<MenuItem>
+				<Link href="/manager/admin/mentors">
+					<div class="flex flex-col items-center gap-1">
+						<User />
+						<p>Mentors</p>
+					</div>
+				</Link>
+			</MenuItem>
+			<MenuItem>
+				<Link href="/manager/admin/maintenance">
+					<div class="flex flex-col items-center gap-1">
+						<Percent />
+						<p>Maintenance</p>
+					</div>
+				</Link>
+			</MenuItem>
+			<MenuItem>
+				<Button onClick={onLogout} withBg={false} textColor="light" withPadding={false}>
+					<div class="flex flex-col items-center gap-1">
+						<LogOut />
+						<p>Logout</p>
+					</div>
+				</Button>
+			</MenuItem>
+		</Menubar>
+	</div>
 </main>
