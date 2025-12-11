0e4c79edca31c595f9b094cf77fd09aaac7b2722
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-12-11 19:44:56 +0700

    fix: fix down migration script

--- backend/db/migrations/000001_create_users_table.down.sql
+++ backend/db/migrations/000001_create_users_table.down.sql
@@ -1,8 +1,9 @@
-DROP INDEX IF EXISTS idx_username_gin, idx_useremail_gin, idx_userpublicid_gin;
-DROP TRIGGER IF EXISTS trigger_set_user_public_id;
-DROP FUNCTION IF EXISTS set_user_public_id;
-DROP FUNCTION IF EXISTS generate_short_id;
+DROP INDEX IF EXISTS idx_username_gin;
+DROP INDEX IF EXISTS idx_useremail_gin;
+DROP INDEX IF EXISTS idx_userpublicid_gin;
+DROP TRIGGER IF EXISTS trigger_set_user_public_id ON users;
+DROP FUNCTION IF EXISTS set_user_public_id();
+DROP FUNCTION IF EXISTS generate_short_id(INT);
 DROP TABLE IF EXISTS users;
 DROP EXTENSION IF EXISTS pg_trgm;
 DROP EXTENSION IF EXISTS pgcrypto;
-
