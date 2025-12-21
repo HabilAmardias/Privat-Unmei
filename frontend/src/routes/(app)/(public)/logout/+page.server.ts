5da67d34866cd2cb017be1b626b2700b051a40b2
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-12-21 20:13:13 +0700

    change logout to server side redirect instead of client side redirect

--- frontend/src/routes/(app)/(public)/logout/+page.server.ts
+++ frontend/src/routes/(app)/(public)/logout/+page.server.ts
@@ -1,9 +1,10 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = ({ cookies }) => {
 	cookies.delete('auth_token', { path: '/' });
 	cookies.delete('refresh_token', { path: '/' });
 	cookies.delete('status', { path: '/' });
-	cookies.delete('role', { path: '/' })
-	return { success: true };
+	cookies.delete('role', { path: '/' });
+	throw redirect(303, '/login');
 };
