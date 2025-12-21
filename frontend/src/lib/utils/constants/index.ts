9a12d77cc677c2862375876ef17467ee55f449e8
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-12-21 20:24:13 +0700

    add custom error page

--- frontend/src/lib/utils/constants/index.ts
+++ frontend/src/lib/utils/constants/index.ts
@@ -1,7 +1,12 @@
-export const FILE_IMAGE_THRESHOLD = 1000 * 1000
-export const MAX_BIO_LENGTH = 250
-export const PrivatUnmeiLogo = 'https://res.cloudinary.com/dk8rlicon/image/upload/v1759932487/PrivatUnmeiLogo_afxh4w.png'
-export const PrivatUnmeiLogoLight = 'https://res.cloudinary.com/dk8rlicon/image/upload/v1760261294/PrivateUnmeiLogoWhite_euk95d.png'
-export const PrivatUnmeiLogoMini = 'https://res.cloudinary.com/dk8rlicon/image/upload/v1760262869/PrivatUnmeiLogoMini_nhmm8i.png'
-export const adminRole = '1'
-export const mentorRole = '2'
+export const FILE_IMAGE_THRESHOLD = 1000 * 1000;
+export const MAX_BIO_LENGTH = 250;
+export const PrivatUnmeiLogo =
+	'https://res.cloudinary.com/dk8rlicon/image/upload/v1759932487/PrivatUnmeiLogo_afxh4w.png';
+export const PrivatUnmeiLogoLight =
+	'https://res.cloudinary.com/dk8rlicon/image/upload/v1760261294/PrivateUnmeiLogoWhite_euk95d.png';
+export const PrivatUnmeiLogoMini =
+	'https://res.cloudinary.com/dk8rlicon/image/upload/v1760262869/PrivatUnmeiLogoMini_nhmm8i.png';
+export const adminRole = '1';
+export const mentorRole = '2';
+export const ErrorLogo =
+	'https://res.cloudinary.com/dk8rlicon/image/upload/v1766323179/Error_Logo_gooyv6.png';
---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import CldImage from '$lib/components/image/CldImage.svelte';
+	import { ErrorLogo } from '$lib/utils/constants';
+</script>
+
+<div class="flex h-dvh w-full items-center justify-center">
+	<div class="flex flex-col items-center">
+		<CldImage src={ErrorLogo} height={400} width={400} />
+		<h1 class="font-bold text-[var(--tertiary-color)]">{page.status}</h1>
+		<p class="text-[var(--tertiary-color)]">{page.error?.message}</p>
+	</div>
+</div>
