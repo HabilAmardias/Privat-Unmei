b1f8234e0588b2c98b31c28d4971073321530fab
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-12-21 21:10:52 +0700

    fixing some styling

--- frontend/src/routes/(public)/verify/[slug]/+page.svelte
+++ frontend/src/routes/(public)/verify/[slug]/+page.svelte
@@ -30,7 +30,7 @@
 {#snippet dialogContent()}{/snippet}
 
 <Dialog buttonText="" bind:open={View.openDialog} title={dialogTitle} description={dialogContent}>
-	<div class="flex w-full flex-col items-center gap-2">
+	<div class="flex w-full flex-col items-center gap-4">
 		<CircleCheck size={128} class="text-[var(--tertiary-color)]" />
 		<p>Your account is now successfully verified</p>
 		<p>You can now login with your account</p>
