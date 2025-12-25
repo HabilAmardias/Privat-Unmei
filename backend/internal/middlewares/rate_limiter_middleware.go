aaf55dee2a25c65261e99532af67fd86169c3581
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2025-12-25 10:28:52 +0700

    whatever

--- backend/internal/middlewares/rate_limiter_middleware.go
+++ backend/internal/middlewares/rate_limiter_middleware.go
@@ -3,16 +3,38 @@ package middlewares
 import (
 	"context"
 	"errors"
+	"os"
 	"privat-unmei/internal/customerrors"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis_rate/v10"
 )
 
 func RateLimiterMiddleware(limiter *redis_rate.Limiter) gin.HandlerFunc {
-	requestRate := 10
+
 	return func(ctx *gin.Context) {
-		res, err := limiter.Allow(context.Background(), ctx.ClientIP(), redis_rate.PerMinute(requestRate))
+		requestRate, ok := os.LookupEnv("REQUEST_RATE")
+		if !ok {
+			ctx.Error(customerrors.NewError(
+				"something went wrong",
+				errors.New("request rate config does not exist"),
+				customerrors.CommonErr,
+			))
+			ctx.Abort()
+			return
+		}
+		requestRateParsed, err := strconv.Atoi(requestRate)
+		if err != nil {
+			ctx.Error(customerrors.NewError(
+				"something went wrong",
+				err,
+				customerrors.CommonErr,
+			))
+			ctx.Abort()
+			return
+		}
+		res, err := limiter.Allow(context.Background(), ctx.ClientIP(), redis_rate.PerMinute(requestRateParsed))
 		if err != nil {
 			ctx.Error(customerrors.NewError(
 				"something went wrong",
