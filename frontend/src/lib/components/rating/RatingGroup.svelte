926e09c8a899e26fc57f320555a8d71a579cd217
Muhammad Habil Amardias <muhammadhabilamardias@Muhammads-MacBook-Air.local>
2026-01-16 13:12:22 +0700

    change rating group to stable version

--- frontend/src/lib/components/rating/RatingGroup.svelte
+++ frontend/src/lib/components/rating/RatingGroup.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { unstable_RatingGroup } from 'bits-ui';
+	import { RatingGroup } from "bits-ui";
 	import { Star } from '@lucide/svelte';
 	type ratingGroupProps = {
 		value: number;
@@ -8,15 +8,15 @@
 	let { value = $bindable(), name }: ratingGroupProps = $props();
 </script>
 
-<unstable_RatingGroup.Root {name} bind:value min={1} max={5} class="flex gap-1">
+<RatingGroup.Root {name} bind:value min={1} max={5} class="flex gap-1">
 	{#snippet children({ items })}
 		{#each items as item (item.index)}
-			<unstable_RatingGroup.Item
+			<RatingGroup.Item
 				index={item.index}
 				class="group size-10 cursor-pointer text-[var(--primary-color)] transition-colors hover:text-[var(--primary-color)] data-[state=inactive]:text-[var(--tertiary-color)] md:size-8"
 			>
 				<Star class="size-full fill-current" />
-			</unstable_RatingGroup.Item>
+			</RatingGroup.Item>
 		{/each}
 	{/snippet}
-</unstable_RatingGroup.Root>
+</RatingGroup.Root>
